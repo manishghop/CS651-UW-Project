ec5ab3b45b8768174d40f4aff8bd4a4cf6bbdb3b
Kamyar Ghajar <k.ghajar@gmail.com>
2020-11-04 02:19:17 +0330

    save tweets to file with user ids

--- save_tweets.py
+++ save_tweets.py
@@ -1,7 +1,6 @@
 import json
 from tweepy import OAuthHandler, Stream, API
 from tweepy.streaming import StreamListener
-import socket
 
 
 class TweetsListener(StreamListener):
@@ -13,9 +12,10 @@ class TweetsListener(StreamListener):
         try:
             tweet = extract_tweet_text(raw_data)
             with open('tweets.txt', 'a') as file:
-                file.write(str(tweet))
+                file.write(json.dumps(tweet)+'\n')
         except Exception as e:
             print('Error on_data: %s' % str(e))
+            print(raw_data)
         return True
     
     def on_error(self, status_code):
@@ -28,11 +28,16 @@ class TweetsListener(StreamListener):
 
 def extract_tweet_text(raw_data):
     data = json.loads(raw_data)
+    if 'lang' in data and data['lang'] != 'en':
+        raise Exception('Wrong tweet language')
+    tweet = {}
     if 'retweeted_status' in data and 'extended_tweet' in data['retweeted_status']:
-        return data['retweeted_status']['extended_tweet']['full_text']
+        tweet['text'] = data['retweeted_status']['extended_tweet']['full_text']
     elif 'extended_status' in data:
-        return data['extended_status']['full_text']
-    return data['text']
+        tweet['text'] = data['extended_status']['full_text']
+    tweet['text'] = data['text']
+    tweet['user_id'] = data['user']['id']
+    return tweet
 
 
 def main():
@@ -41,20 +46,17 @@ def main():
     consumer_key = 'oPBtnqOvoZUSHWrLIA1SLn1Eh'
     consumer_secret = 'CrI5XRUPvRR7fcPWfNLI8JS9DGwmWKw8h6OyjxeO4gF9inZjFw'
     
-    track_array = [
-        'Trump',
-        'Biden',
-        'election'
-    ]
-    
     # Authenticating
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     api = API(auth)
     
     # Establishing the twitter streams
+    stream_track = ['Trump', 'Biden', 'US Elections 2020', '2020election', 'PresidentialElection']
+    us_bbox = [-171.791110603, 18.91619, -66.96466, 71.3577635769]
+
     twitter_stream = Stream(auth, TweetsListener(api), tweet_mode='extended_tweet')
-    twitter_stream.filter(track=track_array, is_async=True, languages=['en'])
+    twitter_stream.filter(track=stream_track, is_async=True, languages=['en'], locations=us_bbox)
 
 
 if __name__ == '__main__':
